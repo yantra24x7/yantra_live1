81ec1d17202b466d4f057a6e615135ce9680d8e5
archana_altius <archana.krishnasamy@adcltech.com>
2020-09-07 14:01:59 +0530

    jen test

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   attr_reader :current_user
   include ExceptionHandler
   private
-
+#Manisankar
   def authenticate_request	
     @current_user = AuthorizeApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user.present?
